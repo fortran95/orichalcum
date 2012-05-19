2d4f1cb2c4ed14dbacba46e0546486b915fe8e68
lucifer <fortran95@126.com>
2012-05-19 16:05:47 +0800

    Modified notifing program.

--- notifier.py
+++ notifier.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from Tkinter import *
 import pynotify
 import time,subprocess
 
@@ -16,15 +17,30 @@ def osd_write(message,timed=450.0):
     ps.stdin.write("""<message id='test' hide_timeout='%d' osd_vposition='top' osd_halignment='center'>\n\n<span foreground='#FF0000'>%s</span></message>""" % (timed,message))
     ps.communicate()
     time.sleep((timed + 150) / 1000)
-def gnotify(message,desc):
+def gnotify(message,desc,callback):
     ns = subprocess.Popen(['mpg123','-q','alarms/caution.mp3'])
     pynotify.init("Orichalcum")
     n = pynotify.Notification(message,desc)
     n.set_urgency(pynotify.URGENCY_NORMAL)
+    #if callback != None:
+        #n.add_action("clicked","Button text", callback, None)
     n.set_timeout(15)
     n.show()
     time.sleep(1)
+def showMessage(sender,message):
+    root = Tk()
+    
+    txt = Text(root,width=40,height=10)
+    txt.insert(END,message)
+    txt.grid(row=0,column=0)
+    
+    root.title("来自 %s 的消息" % sender)
+    root.mainloop()
+    
 if __name__ == '__main__':
     #osd('未读消息：3条')
     #osd('地震速报',['2008年5月12日 午后2时28分','四川省汶川县发生7.8级地震'])
-    gnotify("New message","content" * 64)
+    def callback():
+        print "Clicked!"
+    gnotify("New message","content",callback)
+    showMessage('a','b')
--- processor.py
+++ processor.py
@@ -3,6 +3,7 @@ import notifier
 def handle(message):
     try:
         notifier.gnotify('来自 %s 的新消息' % message['sender'],message['message'])
+        notifier.showMessage(message['sender'],message['message'])
     except:
         pass
     return True # if successfully handled this message.
