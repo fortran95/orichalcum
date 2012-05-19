175f7721263e33b6f5b7aafb9a31472b480a2e5b
lucifer <fortran95@126.com>
2012-05-19 14:38:39 +0800

    Testing start.

--- processor.py
+++ processor.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import notifier
 def handle(message):
-    notifier.gnotify('新消息',message['message'])
+    try:
+        notifier.gnotify('来自 %s 的新消息' % message['sender'],message['message'])
+    except:
+        pass
     return True # if successfully handled this message.
