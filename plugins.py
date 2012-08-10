58b0f92cbddd69022e399183057e89568091148f
lucifer <fortran95@126.com>
2012-08-10 21:06:46 +0800

    Doing plugin interface.

--- plugins.py
+++ plugins.py
@@ -1,8 +1,12 @@
+"""
+{'message': u'a2\n', 'tag': u'akasha', 'more': {u'timestamp': u'1344568248', u'account': u'admin', 'sender': 'li.....ai'}}
+"""
+
 handlers = {}
 
 try:
     import akasha
-    handlers['akasha'] = akasha.handler
+    handlers['akasha'] = (akasha.handler,{'keep-record':False})
 except:
     print "Warning: Akasha cannot be loaded."
 
@@ -11,7 +15,7 @@ def plugin_do(message):
     tag = message['tag']
 
     if handlers.has_key(tag):
-        handlers[tag](message)
+        handlers[tag][0](message,handlers[tag][1])
         return True
     else:
         return False
